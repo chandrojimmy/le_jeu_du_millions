fa25a50a0b93b812fa6e25ac0821f300505a968d
Chandro RAZAFINDRAIBE c2e27 <c2e27>
2024-09-20 14:45:16 +0200

    Create functions for autoloadings the path for namespace

--- src/Utils/autoloaders.php
+++ src/Utils/autoloaders.php
@@ -3,7 +3,26 @@
 namespace Chandro\Jeu20\Utils;
 
 class autoloaders
-{
+ {
+     function myAutoloadingWithPrefix(string $className)
+     {
+         $prefix = "Chandro/Jeu20\\";
+         $baseDir =__DIR__.'/../';
+         $lengthPrefix = strlen($prefix);
+         if (strcmp($prefix,$className,$lengthPrefix) !==0){
+             return;
+         }
+         $pathTemp = str_replace($prefix,$baseDir,$className);
+         $pathToClassFile  = str_replace('\\', '/' ,$pathTemp).'.php';
+         if (file_exists($pathToClassFile)){
+             require_once $pathToClassFile;
+         }else{
+             echo "non Trouvé !<br><br>";
+         }
+
+
+     }
+
 
 
 }
